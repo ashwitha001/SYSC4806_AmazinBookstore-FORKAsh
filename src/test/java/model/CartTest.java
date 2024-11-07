0ca7205fad3462880fb0d4d0cdf55383d0ae2ba3
Keefer Belanger <keefer.belanger@gmail.com>
2024-11-07 14:54:46 -0500

    test

--- src/test/java/model/CartTest.java
+++ src/test/java/model/CartTest.java
@@ -1,4 +1,45 @@
+package model;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
+
 public class CartTest {
-  
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getUser() {
+    }
+
+    @Test
+    public void setUser() {
+    }
+
+    @Test
+    public void getItems() {
+    }
+
+    @Test
+    public void setItems() {
+    }
+
+    @Test
+    public void addItem() {
+    }
+
+    @Test
+    public void removeItem() {
+    }
 }
