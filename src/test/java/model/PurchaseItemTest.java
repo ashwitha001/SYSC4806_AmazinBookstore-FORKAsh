e7c1f6574e2befa554a37ba1f0d8d1a98ea93b1e
Keeferf <keeferbelanger@cmail.carleton.ca>
2024-11-09 14:15:12 -0500

    purchaseitemtest

--- src/test/java/model/PurchaseItemTest.java
+++ src/test/java/model/PurchaseItemTest.java
@@ -7,39 +7,79 @@ import static org.junit.Assert.*;
 
 public class PurchaseItemTest {
 
+    private PurchaseItem purchaseItem;
+    private Book book;
+    private Checkout checkout;
+
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
+        book = new Book(
+            "Test Title",
+            "Test Description",
+            "Test Author",
+            "Test Publisher",
+            "http://example.com/test.jpg",
+            10.00,
+            10
+        );
+        checkout = new Checkout();
+        purchaseItem = new PurchaseItem(book, 2, checkout);
     }
 
     @Test
     public void getId() {
+        Long id = 1L;
+        purchaseItem.setId(id);
+        assertEquals(id, purchaseItem.getId());
     }
 
     @Test
     public void setId() {
+        Long id = 5L;
+        purchaseItem.setId(id);
+        assertEquals(id, purchaseItem.getId());
     }
 
     @Test
     public void getBook() {
+        assertEquals(book, purchaseItem.getBook());
     }
 
     @Test
     public void setBook() {
+        Book newBook = new Book(
+            "New Title", 
+            "New Description", 
+            "New Author",  
+            "New Publisher", 
+            "http://newurl.com", 
+            25.99, 
+            15);
+        purchaseItem.setBook(newBook);
+        assertEquals(newBook, purchaseItem.getBook());
     }
 
     @Test
     public void getQuantity() {
+        assertEquals((Integer) 2, purchaseItem.getQuantity());
     }
 
     @Test
     public void setQuantity() {
+        Integer newQuantity = 5;
+        purchaseItem.setQuantity(newQuantity);
+        assertEquals(newQuantity, purchaseItem.getQuantity());
     }
 
     @Test
     public void getPurchase() {
+        assertEquals(checkout, purchaseItem.getPurchase());
     }
 
     @Test
     public void setPurchase() {
+        Checkout newCheckout = new Checkout();
+        purchaseItem.setPurchase(newCheckout);
+        assertEquals(newCheckout, purchaseItem.getPurchase());
     }
 }
